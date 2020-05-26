99839d7d1ddd5c4310d900510887e4614be50ab5
Kravchan <danilka232001@gmail.com>
2020-05-27 01:19:03 +0700

    fun

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
 
-static int t = 1;
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine eng(seed);
 
-int main()
+using namespace std;
+int main ()
 {
-    if (t)
-        printf("on this line\n");
-    else
-        printf("but not here\n");
 
+    int n;
+    cin >>n;
+    std::vector<int> a;
+
+    a.reserve(64);
+    a.resize(n);
+    for (int i=0;i<n;i++){
+    	a[i]=i+1;
+    }
+    for (int i=0;i<n;i++){
+    	cout<<a[i]<<"   ";
+    }
+    cout<<"\n";
+	std::shuffle(std::begin(a), std::end(a), eng);
+    for (int i=0;i<n;i++){
+    	cout<<a[i]<<"   ";
+    }
+    cout<<"\n";
     return 0;
 }
